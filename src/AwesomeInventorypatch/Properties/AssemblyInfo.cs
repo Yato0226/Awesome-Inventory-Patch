5ffe06fa03866776da321e416eb41d59f741526c
Louize26 <90812784+Yato0226@users.noreply.github.com>
2025-01-04 01:35:43 +0800

    1

---
+++ src/AwesomeInventorypatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AwesomeInventorypatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AwesomeInventorypatch")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7e50779-a080-4f58-b22b-031e3b55602f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/AwesomeInventorypatch/main.cs
@@ -0,0 +1,23 @@
+﻿using AwesomeInventory.Loadout;
+using HarmonyLib;
+using Verse;
+
+public class AwesomeInventorypatch : Mod
+{
+    public AwesomeInventorypatch(ModContentPack content) : base(content)
+    {
+        var harmony = new Harmony("com.louize.AwesomeInventorypatch");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(CompAwesomeInventoryLoadout), "get_NeedRestock")]
+public static class Patch_AwesomeInventoryLoadout_NeedRestock
+{
+    public static bool Prefix(ref bool __result)
+    {
+        // Skip the original method and set the result to false
+        __result = false; // Set to false or any other value you want to return
+        return false; // Skip the original method
+    }
+}
